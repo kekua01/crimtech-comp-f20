13fd9f64016fc42d3c75900313fced53422dd1dc
Coby Sheehan <cobysheehan@Skynet.local>
2020-10-22 21:51:25 -1000

    finished yoda lab

--- yoda/lab2-responses.md
+++ yoda/lab2-responses.md
@@ -3,7 +3,15 @@ Yoda Chat: Responses
 
 B. Inspect Mode
 ---------------
+1. chocolate
+2. next_week, "Guest talk?"
+3. z.moatads.com
 
 
 C. Welcome to Programming in Javascript
 ---------------------------------------
+3. 'includes' checks to see if its input is a substring of y. 'repeat' concatenates the string y with itself some x number of times
+
+4. 6.25, NaN
+
+5.
--- yoda/response_handler.js
+++ yoda/response_handler.js
@@ -1,3 +1,4 @@
+
 // Declaring variables that you may want to use.
 let names = ['cute', 'regular'];
 let moods = ['dark', 'force', 'std'];
@@ -18,8 +19,52 @@ let std_quotes = ["Patience you must have, my young padawan.",
 "Judge me by my size, do you?",
 "Difficult to see. Always in motion is the future."
 ];
+// some baby yoda quotes
+let baby_quotes = ["Frogs", "Mando", "Mwahhh", "Yes"]
 
 function respond() {
-    // Your Code Here
-    console.log("Hello World!");
+	var image = "";
+	var quote = "";
+	var hm = "m".repeat(2 + Math.floor(Math.random() * 13));
+
+	message = document.getElementById("chat-box");
+	chat = message.value;
+	console.log(chat);
+
+	//check to see if the words 'force' and 'dark' are in the chat message
+	var force = chat.includes('force');
+	var dark = chat.includes('dark');
+
+	//flowchart
+	if (chat.includes('cute') || chat.includes('baby')) {
+		//pick a random thing to say from baby quotes
+		quote = baby_quotes[Math.floor(Math.random() * baby_quotes.length)] + ", " + hm;
+		if (force) {
+			image = dark ? "img/cute-dark.jpg" : "img/cute-force.jpg";
+		}
+		else {
+			image = "img/cute-std.jpg"
+		}
+	}
+	else {
+		quote = "H" + hm + ". "
+		if (force) {
+			if (dark) {
+				image = "img/regular-dark.jpg";
+				quote += dark_quotes[Math.floor(Math.random() * dark_quotes.length)]
+			}
+			else {
+				image = "img/regular-force.jpg";
+				quote += force_quotes[Math.floor(Math.random() * force_quotes.length)]
+			}
+		}
+		else {
+			image = "img/regular-std.jpg"
+			quote += std_quotes[Math.floor(Math.random() * std_quotes.length)]
+		}
+	}
+ 	document.getElementById("yoda-pic").setAttribute("src", image);
+    document.getElementById("yoda-quote").innerHTML = quote;
+   	message.value = "";
 }
+
